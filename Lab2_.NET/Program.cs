295a1a5d8c1bd74c81d29f43e3f7f0cd7d06f683
Maks Pechkyr <88410682+maAksss@users.noreply.github.com>
2026-02-12 15:01:55 +0200

    Add files via upload

---
+++ Lab2_.NET/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {   Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Assembly asm = Assembly.LoadFrom("MyMathDll.dll");
+
+        object obj = asm.CreateInstance("vscode");
+        Type type = asm.GetType("vscode");
+
+        // ---
+        Console.Write("Введіть число для факторіалу: ");
+        int n = int.Parse(Console.ReadLine());
+
+        MethodInfo factorialMethod = type.GetMethod("Factorial");
+
+        object[] factParams = new object[] { n };
+
+        Console.WriteLine("Факторіал = " +
+            factorialMethod.Invoke(obj, factParams));
+
+        // ---
+        Console.WriteLine("\nВведіть сторони трикутника:");
+
+        Console.Write("a = ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("b = ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("c = ");
+        double c = double.Parse(Console.ReadLine());
+
+        MethodInfo areaMethod = type.GetMethod("TriangleArea");
+
+        object[] areaParams = new object[] { a, b, c };
+
+        double area = (double)areaMethod.Invoke(obj, areaParams);
+
+        if (area == -1)
+            Console.WriteLine("Трикутник не існує!");
+        else
+            Console.WriteLine("Площа трикутника = " + area);
+
+        Console.ReadLine();
+    }
+}
---
+++ MyMathDll/MyMathDll/Class1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+public class vscode
+{
+    public long Factorial(int n)
+    {
+        if (n < 0)
+            return -1;
+
+        long result = 1;
+
+        for (int i = 1; i <= n; i++)
+            result *= i;
+
+        return result;
+    }
+
+    public double TriangleArea(double a, double b, double c)
+    {
+        if (a + b <= c || a + c <= b || b + c <= a)
+            return -1;
+
+        double p = (a + b + c) / 2;
+
+        return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
